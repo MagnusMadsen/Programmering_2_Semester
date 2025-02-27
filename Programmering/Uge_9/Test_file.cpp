fedccc3156904538cec302d6d45fca335ed9f214
MagnusMadsen <40494689+MagnusMadsen@users.noreply.github.com>
2025-02-27 08:46:32 +0000

    update

--- Programmering/Uge_9/Test_file.cpp
+++ Programmering/Uge_9/Test_file.cpp
@@ -1,5 +1,19 @@
 #include <Catch2_headerfile.hpp>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+
+void setup() { 
+
+Serial.print("hej");
+    
+
 }
+
+
+void loop (){ 
+
+
+
+
+}
+
+
